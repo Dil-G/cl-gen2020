c1519c5699482cd08d1848bc2f809b2acbca1952
Medani Gunathilaka <56575775+MedaniGunathilaka@users.noreply.github.com>
2021-01-12 10:58:07 +0530

    1/12

---
+++ public/teacher/csv_mysql1.php
@@ -0,0 +1,40 @@
+<?php  
+$connect = mysqli_connect("localhost", "root", "", "testing");
+if(isset($_POST["submit"]))
+{
+ if($_FILES['file']['name'])
+ {
+  $filename = explode(".", $_FILES['file']['name']);
+  if($filename[1] == 'csv')
+  {
+   $handle = fopen($_FILES['file']['tmp_name'], "r");
+   while($data = fgetcsv($handle))
+   {
+    $item1 = mysqli_real_escape_string($connect, $data[0]);  
+                $item2 = mysqli_real_escape_string($connect, $data[1]);
+                $query = "INSERT into excel(excel_name, excel_email) values('$item1','$item2')";
+                mysqli_query($connect, $query);
+   }
+   fclose($handle);
+   echo "<script>alert('Import done');</script>";
+  }
+ }
+}
+?>  
+<!DOCTYPE html>  
+<html>  
+ <head>  
+  <title>Webslesson Tutorial</title>
+ </head>  
+ <body>  
+  <h3 align="center">How to Import Data from CSV File to Mysql using PHP</h3><br />
+  <form method="post" enctype="multipart/form-data">
+   <div align="center">  
+    <label>Select CSV File:</label>
+    <input type="file" name="file" />
+    <br />
+    <input type="submit" name="submit" value="Import" class="btn btn-info" />
+   </div>
+  </form>
+ </body>  
+</html>
