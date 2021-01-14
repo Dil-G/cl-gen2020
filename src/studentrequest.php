f70209204e69f793386a506ff7e89227d2e14c55
Medani Gunathilaka <56575775+MedaniGunathilaka@users.noreply.github.com>
2021-01-14 14:29:26 +0530

    1/14

---
+++ src/studentrequest.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once '../config/conn.php';
+
+if (isset($_POST['add_request'])){
+
+    
+        $id = $_POST['id'];
+        $name = $_POST['name'];
+        $request= $_POST['request'];
+        $image = $_FILES['image']['name'];
+        $target = "../images/" . basename($image);
+    
+        $date = date('Y-m-d');
+        $time = date('H:i:s');
+
+        $sql = "INSERT INTO Request(id,name,request,image,requestDate,requestTime)VALUES ('$id','$name','$request', '$image','$date','$time')";
+
+        if(mysqli_query($conn,$sql)){
+            if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
+                $message = "Image uploaded successfully";
+                header('Location: ../public/office/o_viewReq.php?message='.$message);
+            }else{
+                header('Location: ../public/office/o_viewReq.php');
+            }
+        }else{
+            $error = "News not added.";
+            header('Location: ../public/teacher/editRequest.php.php?error='.$error);
+        }
+  
+    }else{
+        header('Location: ../public/teacher/editRequest.php?error='.$error);
+    }
+
+$conn->close();
+
+?>
