90f9b1a999acfc5e863888a3ed5f13c54aa06f9c
Medani Gunathilaka <56575775+MedaniGunathilaka@users.noreply.github.com>
2021-02-05 09:32:52 +0530

    2/5

--- public/teacher/Tcr_achievement2.php
+++ public/teacher/Tcr_achievement2.php
@@ -19,8 +19,13 @@
     <title>Achievements</title>
     <script src="../js/jquery-1.9.1.min.js"></script>
     <script src="../js/nav.js"></script>
-    <link rel="stylesheet" href="../css/register2.css " type="text/css">
+    <script src="../js/errors.js"></script>
     <link type="text/css" rel="stylesheet" href="../css/main.css">
+    <link type="text/css" rel="stylesheet" href="../css/register.css">
+    <link type="text/css" rel="stylesheet" href="../css/register2.css">
+    <link type="text/css" rel="stylesheet" href="../css/view.css">
+    <link type="text/css" rel="stylesheet" href="../css/register.css">
+    <link type="text/css" rel="stylesheet" href="../css/messages.css">
 </head>
 
 <body name=top>
@@ -29,54 +34,59 @@
 
 
         <div id="nav3"></div>
+       
         <div class="content">
             <div class="container">
-                <form action="../php/register.php" method="POST">
-                    <h1>Achievements</h1>
+            <form action="../../src/add_achievement2.php"  onsubmit="return validateUsername()" method="POST" enctype="multipart/form-data">
+                <h1 style="color:#6a7480;">Achievement Form</h1>
                     <hr>
 
                     <label for="name"><b>Student Admission Number</b></label>
-                    <input type="text" placeholder="Enter admission number" name="anumber" required>
-
-                    <label for="name"><b>Achievement ID</b></label>
-                    <input type="text" placeholder="Enter achievement ID" name="aID" required>
-
+                    <input type="text" placeholder="Enter admission number"  id="username" name="anumber"  onblur="validateUsername(username.value)"  required>
+                  
+                    
                     <label for="nic"><b>Category ID</b></label>
                     <input type="text" placeholder="Enter category ID" name="cID" required>
 
-                    <label for="email"><b>Achievement Date</b></label>
-                    <input type="text" placeholder="Enter achievement date" name="aDate" required>
-
-                    <label for="dob"><b>Date of Enter</b></label>
-                    <input type="text" placeholder="Enter date of enter" name="dateofenter" required>
-
+                    <label for="aDate"><b>Achievement Date</b></label>
+                    <input type="date" placeholder="Enter achievement date" name="aDate" id="date" required>
+                    
                     <label for="position"><b>Achievement Name</b></label>
                     <input type="text" placeholder="Achievement name" name="aname" required>
 
-                    <label for="position"><b>Position</b></label>
-                    <input type="text" placeholder="Enter position" name="position" required>
 
-                    <label for="position"><b>Description</b></label>
+
+                    <label for="position"><b>Position</b></label>
+                    <select name="position" id="district" required>
+                    <option disabled selected value> -- Select an option -- </option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="patticipation">Participation</option>
+                        </select>
+                    <br><br>
+
+                    <label for="description"><b>Description</b></label>
                     <input type="text" placeholder="Enter description" name="description" required>
 
                     <label><b>Important Value:</b></label>
                     <label> <input type="radio" name="Ivalue" value="1">1</label>
-
                     <label> <input type="radio" name="Ivalue" value="2">2</label>
                     <label><input type="radio" name="Ivalue" value="3">3</label>
 
                     <br>
                     </br><br>
                     </br>
-                    <hr>
-
-                    <button type="submit" class="registerbtn">ADD</button>
-                    <hr>
-                </form>
+                  
+                    <br>
+                <div id="msg"></div>
                 <hr>
-                <h2 align="center"><a href="Tcr_dashboard3.php">Cancel</a></h2>
-                <hr>
+                <div>
+                    <button type="submit" class="registerbtn" name="regbtn">Add</button>
+                    <a href="Tcr_dashboard3.php" class="cancel-btn">Cancel</a>
+              
             </div>
+        </div>
         </div>
     </body>
 
@@ -85,3 +95,8 @@
 <?php 
 	 }
 ?>
+       
+       
+       
+       
+       
