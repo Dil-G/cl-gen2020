61f0f2b64db41a953d8b675dd82b1f44c5e93e8f
Medani Gunathilaka <56575775+MedaniGunathilaka@users.noreply.github.com>
2021-02-13 20:15:10 +0530

    2/13-new

--- public/teacher/Tcr_RequestEdits.php
+++ public/teacher/Tcr_RequestEdits.php
@@ -1,15 +1,16 @@
 <?php
-     session_start();
+    session_start();
 
-     if(!isset($_SESSION['userType']) && !isset($_SESSION['userID'])){
-         $error = "Please Login!";
-         header('Location: ../common/loginFile.php?error='.$error);
-        }else if($_SESSION['userType'] != 'teacher'){
-            header('Location: ../common/error.html');
-     }else if(($_SESSION['userType'] == 'teacher') && ($_SESSION['teacherType'] == 'classTcr')){
+    if(!isset($_SESSION['userType']) && !isset($_SESSION['userID'])){
+        $error = "Please Login!";
+        header('Location: ../common/loginFile.php?error='.$error);
+    }elseif($_SESSION['userType'] == 'teacher'){
+      
+      $teacherType = array();
+      $teacherType = $_SESSION['teacherType'];
 
-         $userID = $_SESSION['userID'];
-?>
+     
+	?>
 
 
 <!DOCTYPE html>
@@ -30,7 +31,7 @@
 
 <body>
 
-    <div id="nav1"></div>
+    <div id="teacherNav"></div>
 
     
     <div class="content">
